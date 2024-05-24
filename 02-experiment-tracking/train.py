58bff744f6ec9864d2395793b4b8352e01dd7d1d
Vladimir Zhelanov <vzhelanov@aligntech.com>
2024-05-25 00:42:49 +0300

    Homework for 02-experiment-tracking

---
+++ 02-experiment-tracking/train.py
@@ -0,0 +1,34 @@
+import os
+import pickle
+import click
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+
+import mlflow
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+        rmse = root_mean_squared_error(y_val, y_pred)
+
+
+if __name__ == '__main__':
+    mlflow.sklearn.autolog()
+    run_train()
